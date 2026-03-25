8b3c1c3fd1c79c4202064710629929e7b6f09a4a
Vishesh394 <visheshkumar392004@gmail.com>
2026-03-26 04:56:32 +0530

    Done with all the concepts of EJS and Started working on project

--- frontendUI/express-ejs-app/app.js
+++ frontendUI/express-ejs-app/app.js
@@ -1,17 +1,32 @@
-const express=require("express")
-const app=express()
+const express = require("express")
+const path = require("path")
+const app = express()
 
 //view from MVC: Model View Controller
 //for small level: we can
-app.set('view engine','ejs')
-app.get("/", (req,res)=>{
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist')))
+
+app.get("/", (req, res) => {
     // res.render('index',{fullname: "Pankaj"})
     // res.render('index',{isLoggedIn:true})  //conditions 
 
     //passing array instead of a normal variable:
-    res.render('index',
-    {isLoggedIn:true},
-    {users:['rahul','vikas','ajay','rohan']})
+    res.render('index', { users: ['rahul', 'vikas', 'ajay', 'rohan'] })
 
 })
-app.listen(5000,()=>console.log("Server is running on http://localhost:3000"))
+
+app.get("/about", (req, res) => {
+    res.render('about')
+})
+
+app.get("/services", (req, res) => {
+    res.render('services')
+})
+
+app.get("/contact", (req, res) => {
+    res.render('contact')
+})
+
+app.listen(3000, () => console.log("Server is running on http://localhost:3000"))
